34879001b863b78b1297afcbb07be00168817ec2
Jayrek <jrektabasa@gmail.com>
2025-02-26 17:51:15 +0800

    update product list screen

---
+++ lib/views/screens/product_list_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../core/constants/enums/product_fetch_status.dart';
+import '../../models/category.dart';
+import '../../viewmodels/product/product_bloc.dart';
+import '../widgets/category_panel_widget.dart';
+import '../widgets/product_panel_widget.dart';
+
+class ProductListScreen extends StatelessWidget {
+  const ProductListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: Text(
+          'FakeStoreApp',
+          style: Theme.of(context)
+              .textTheme
+              .displayLarge
+              ?.copyWith(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepOrange,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      drawer: const Drawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BlocBuilder<ProductBloc, ProductState>(
+              builder: (context, state) {
+                List<Category> categories = state.categories;
+                return CategoryPanelWidget(categories: categories);
+              },
+            ),
+            BlocConsumer<ProductBloc, ProductState>(
+              listener: (context, state) {
+                if (state.productFetchState == ProductFetchState.failed &&
+                    state.errorMessage.isNotEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(state.errorMessage)));
+                }
+              },
+              builder: (context, state) {
+                final products = state.products;
+                return ProductPanelWidget(
+                  products: products,
+                  productState: state,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
