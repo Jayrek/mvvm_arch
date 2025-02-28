f4479c920314711b41cfd18fc94174a58695c4e0
Jayrek <jrektabasa@gmail.com>
2025-02-28 22:14:30 +0800

    add initial related cart bloc

---
+++ lib/viewmodels/cart/cart_state.dart
@@ -0,0 +1,10 @@
+part of 'cart_bloc.dart';
+
+class CartState extends Equatable {
+  const CartState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class CartInitial extends CartState {}
