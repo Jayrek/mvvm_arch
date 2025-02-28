6f9aa99d11462bf6a1ab276acce570712c016a2d
Jayrek <jrektabasa@gmail.com>
2025-03-01 07:10:51 +0800

    udpate main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mvvm_arch/core/services/fake_store_service.dart';
-// import 'package:mvvm_arch/viewmodels/category/category_bloc.dart';
+import 'package:mvvm_arch/core/theme/app_theme.dart';
 import 'package:mvvm_arch/viewmodels/product/product_bloc.dart';
 import 'package:mvvm_arch/views/screens/product_detail_screen.dart';
 import 'package:mvvm_arch/views/screens/product_list_screen.dart';
 
-// import 'models/product.dart';
+import 'core/constants/constant_string.dart';
+import 'viewmodels/cart/cart_bloc.dart';
+import 'views/screens/cart_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,46 +30,24 @@ class MyApp extends StatelessWidget {
                   ..add(const FetchProductList())
                   ..add(const FetchCategoryList()),
           ),
-          // BlocProvider(
-          //   create: (context) =>
-          //       CategoryBloc(fakeStoreService: context.read<FakeStoreService>())
-          //         ..add(const FetchCategory()),
-          // ),
+          BlocProvider(
+            create: (context) =>
+                CartBloc(fakeStoreService: context.read<FakeStoreService>())
+                  ..add(const FetchCartProductList()),
+          ),
         ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'FakeStoreApp',
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-            textTheme: const TextTheme(
-              displayLarge: TextStyle(
-                fontFamily: 'Montserrat',
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-              titleMedium: TextStyle(
-                fontFamily: 'Montserrat',
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
-              ),
-              bodyMedium: TextStyle(
-                fontFamily: 'Lato',
-                fontSize: 16,
-                color: Colors.black87,
-              ),
-              bodySmall: TextStyle(
-                fontFamily: 'Lato',
-                fontSize: 14,
-                color: Colors.black87,
-              ),
-            ),
-          ),
-          initialRoute: '/',
+          title: ConstantString.appName,
+          theme: AppTheme.lightTheme,
+          initialRoute: ConstantString.navigationIndex,
           routes: {
-            '/': (context) => const ProductListScreen(),
-            '/productDetail': (context) => const ProductDetailScreen()
+            ConstantString.navigationIndex: (context) =>
+                const ProductListScreen(),
+            ConstantString.navigationProductDetail: (context) =>
+                const ProductDetailScreen(),
+            ConstantString.navigationCartList: (context) =>
+                const CartListScreen(),
           },
           // onGenerateRoute: (settings) {
           //   if (settings.name == '/productDetail') {
