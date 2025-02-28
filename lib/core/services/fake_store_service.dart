14c751bc5a06d8b7c6f06c013eb4fcf1bd8ca4d9
Jayrek <jrektabasa@gmail.com>
2025-03-01 07:09:06 +0800

    fetch cart products

--- lib/core/services/fake_store_service.dart
+++ lib/core/services/fake_store_service.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:mvvm_arch/models/product.dart';
 
+import '../../models/cart.dart';
 import '../errors/failure.dart';
 
 class FakeStoreService {
@@ -74,6 +75,24 @@ class FakeStoreService {
         final data = response.data;
         final product = Product.fromJson(data);
         return Right(product);
+      }
+      return Left(ServerFailure(response.statusCode.toString()));
+    } catch (e) {
+      if (e is DioException) {
+        return Left(NetworkFailure(e.toString()));
+      }
+      return Left(UnknownFailure(e.toString()));
+    }
+  }
+
+  // fetching cart items
+  Future<Either<Failure, Cart>> getCartItems() async {
+    try {
+      final response = await dio.get('$_baseUrl/carts/1');
+      if (response.statusCode == 200) {
+        final data = response.data;
+        final cart = Cart.fromJson(data);
+        return Right(cart);
       }
       return Left(ServerFailure(response.statusCode.toString()));
     } catch (e) {
