b6a21bb40c9e1777728145ee1d3b4bcb12e241f2
Jayrek <jrektabasa@gmail.com>
2025-03-01 07:08:49 +0800

    add new constant strings

--- lib/core/constants/constant_string.dart
+++ lib/core/constants/constant_string.dart
@@ -1,1 +1,27 @@
-const dollarSign = '\$';
+class ConstantString {
+  // fonts
+  static const String fontMontserrat = 'Montserrat';
+  static const String fontLato = 'Lato';
+
+  // strings
+  static const String appName = 'FakeStoreApp';
+  static const String addToCart = 'Add To Cart';
+  static const String buyNow = 'Buy Now';
+  static const String dollarSign = '\$';
+  static const String prdoucts = 'Products';
+  static const String cart = 'Cart';
+  static const String about = 'About';
+  static const String totalPrice = 'Total Price: ';
+  static const String shopMore = 'Shop More';
+  static const String checkOut = 'CheckOut';
+  static const String noProducts = 'No products available!';
+  static const String notYetAvailable = 'Not yet available!';
+  static const String reviewItem = 'Review your items before checkout!';
+  static const String reviewItemBeforeProceeding =
+      'Make sure everything is correct before proceeding.';
+
+  // navigation keys
+  static const String navigationIndex = '/';
+  static const String navigationProductDetail = '/productDetail';
+  static const String navigationCartList = '/cartList';
+}
